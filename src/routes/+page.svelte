7b48fa6e4d08bae51bee22ede784730c65e4bfb9
astoria <astoriabublas@gmail.com>
2024-10-19 02:00:19 +0200

    +done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import Item from "$lib/components/Item.svelte";
+	import Summary from "$lib/components/Summary.svelte";
 	let items: any[] = [];
 	let money = 0
+	let done = false
 
 	function add() {
 		items = [...items, {
@@ -20,14 +22,23 @@
 		item.value = event.detail.value
 		sum()
 	}
+
+	function submit() {
+		done = true
+	}
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5 pt-8 w-96">
 		<h1 class="h1">Money: {money}</h1>
-		{#each items as item}
-			<Item defaultValue={item.value} id={item.id} on:change={change}></Item>
-		{/each}
-		<button class="p-4 w-full text-center bg-primary-500 text-xl font-bold" on:click={add}>+</button>
+		{#if !done}
+			{#each items as item}
+				<Item defaultValue={item.value} id={item.id} on:change={change}></Item>
+			{/each}
+			<button class="p-4 w-full text-center bg-primary-500 text-xl font-bold" on:click={add}>+</button>
+			<button class="p-4 w-full text-center bg-secondary-500 text-xl font-bold" on:click={submit}>=</button>
+		{:else}
+			<Summary orders={items.map(i => parseFloat(i.value))}></Summary>
+		{/if}
 	</div>
 </div>
